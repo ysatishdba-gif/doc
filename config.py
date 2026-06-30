a96f6978df591b31f0fa89f4c31c2870f1875f14
ysatishdba-gif <ysatishdba@gmail.com>
2026-06-30 12:43:32 -0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-cui_dict_path = "cui_def_dict/cui_definitions.json"
-temporal_norm_code_json = "Normalized_loinc_classes/temporal_norm_code_to_formula.json"
-doctype_norm_code_json = "Normalized_loinc_classes/doctype_norm_code_to_loinc_codes_dict.json"
-
-shopping_list_team = "cohort-based"
-nb_results_json = "Nature_breakdown/DT-nature-breakdown/epic-cohort-analysis.json"
